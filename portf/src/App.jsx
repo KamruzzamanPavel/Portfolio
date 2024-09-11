abd7ebf949b11739090e3aca227c5bc5007ac507
KamruzzamanPavel <kamruzzamanpavel88@gmail.com>
2024-09-11 19:37:26 +0600

    enhanced a lot and lot4

--- portf/src/App.jsx
+++ portf/src/App.jsx
@@ -5,16 +5,15 @@ import NavBar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Education from "./components/Education";
 import PersonalInfo from "./components/PersonalInfo";
-import ScrollButton from "./ScrollButton";
+import ScrollButton from "./components/ScrollButton";
 const App = () => {
   return (
     <div
       id="skills"
       className="min-h-screen bg-gray-300 dark:bg-gray-900 text-gray-900 dark:text-gray-100"
     >
-      <NavBar />
-
       <main className="p-4">
+        <NavBar />
         <About />
         <PersonalInfo />
         <Projects />
